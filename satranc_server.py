c78db62125238673b6e8fce2dae1ae688ce07a3c
SemsYapar <67692805+SemsYapar@users.noreply.github.com>
2026-06-06 16:59:49 +0300

    Update Stockfish move handling in API

--- satranc_server.py
+++ satranc_server.py
@@ -1,7 +1,6 @@
 from stockfish import Stockfish
 from flask import Flask, request, json, send_file
 import random, os
-#stockfish = Stockfish("C:\\Users\\safak\\Masaüstü\\stockfish_15_win_x64_avx2\\stockfish_15_win_x64_avx2\\stockfish_15_x64_avx2.exe",depth=20,parameters={"Threads": 5, "Minimum Thinking Time": 20, "Hash":2048, "Ponder":"true"})
 
 stockfish = Stockfish(os.path.abspath("stockfish_10_x64.exe"))
 api = Flask(__name__)
@@ -10,7 +9,7 @@ api = Flask(__name__)
 def get_best_move():
     pos = request.form.get("pos").split()
     depth = request.form.get("depth")
-    stockfish.set_position(pos)
+    stockfish.make_moves_from_start(pos)
     stockfish.set_depth(int(depth))
     return stockfish.get_best_move()
 
@@ -18,7 +17,7 @@ def get_best_move():
 def get_top5_move():
     pos = request.form.get("pos").split()
     depth = request.form.get("depth")
-    stockfish.set_position(pos)
+    stockfish.make_moves_from_start(pos)
     stockfish.set_depth(int(depth))
     return stockfish.get_top_moves(5)
 
